3a8414730c346e4ff21e94223cd68e9b439157c4
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-31 17:37:19 -0400

    add timestamps to photos

--- db/migrate/20151031213415_create_photos.rb
+++ db/migrate/20151031213415_create_photos.rb
@@ -3,6 +3,8 @@ class CreatePhotos < ActiveRecord::Migration
     create_table :photos do |t|
       t.integer :interest_point_id
       t.string :name
+
+      t.timestamps null: false
     end
   end
 end
