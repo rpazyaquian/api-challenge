f9b76f1843863dce038312addc9b8f20c9ff1683
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-31 14:34:05 -0400

    fix migration errors

--- db/migrate/20151031182517_create_listings.rb
+++ db/migrate/20151031182517_create_listings.rb
@@ -1,8 +1,20 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
+      t.string :uuid
+      t.boolean :active
+      t.string :name
+      t.string :email
+      t.string :description
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.string :lat
+      t.string :lng
+      t.string :phone_number
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
