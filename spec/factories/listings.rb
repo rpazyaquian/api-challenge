aaff49f501d1b26d1df043799ad0fc857e6b4bb1
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-31 14:46:01 -0400

    update listing factory

--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -1,17 +1,17 @@
 FactoryGirl.define do
   factory :listing do
-    uuid Faker::Bitcoin.address
+    uuid { Faker::Bitcoin.address }
     active true
-    name Faker::Name.name
-    email Faker::Email.email
-    description Faker::Lorem.sentence
-    address Faker::Address.street_address
-    city Faker::Address.city
-    state Faker::Address.state_abbr
-    zipcode Faker::Address.zip
-    lat Faker::Address.latitude
-    lng Faker::Address.longitude
-    phone_number Faker::PhoneNumber.phone_number
+    name { Faker::Name.name }
+    email { Faker::Email.email }
+    description { Faker::Lorem.sentence }
+    address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zipcode { Faker::Address.zip }
+    lat { Faker::Address.latitude }
+    lng { Faker::Address.longitude }
+    phone_number { Faker::PhoneNumber.phone_number }
   end
 
 end
