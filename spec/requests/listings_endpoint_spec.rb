96b70372da21849da8e534f891db78587c77c325
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-31 15:52:15 -0400

    filled out POST spec for listings

--- spec/requests/listings_endpoint_spec.rb
+++ spec/requests/listings_endpoint_spec.rb
@@ -2,13 +2,23 @@ require "rails_helper"
 
 RSpec.describe "Listings endpoint", :type => :request do
   describe "/index" do
-    let!(:listing) do
-      FactoryGirl.create(:listing)
+    describe "GET" do
+      let!(:listing) do
+        FactoryGirl.create(:listing)
+      end
+      it "shows all listings" do
+        get "/listings"
+        expect(JSON.parse(response.body).length).to eq 1
+      end
     end
-    it "shows all listings" do
-      get "/listings"
-      puts response.body
-      expect(response).to be_success
+    describe "POST" do
+      let(:listing_attrs) do
+        FactoryGirl.attributes_for(:listing)
+      end
+      it "creates a new listing" do |variable|
+        post "/listings", params: listing_attrs
+        expect(response).to be_success
+      end
     end
   end
 end
