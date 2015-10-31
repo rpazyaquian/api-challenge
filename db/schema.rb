2f6ee6aa396847d0aa54ec02aff7ad3542b2100f
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-31 17:34:44 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031182517) do
+ActiveRecord::Schema.define(version: 20151031213415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20151031182517) do
     t.string   "phone_number"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer "interest_point_id"
+    t.string  "name"
   end
 
 end
