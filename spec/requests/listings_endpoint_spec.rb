b28b5549918719dab4bb6df38d0346195e312a29
Rebecca Paz <r.pazyaquian@gmail.com>
2015-11-02 10:27:02 -0500

    create fixtures for API requirements spec

--- spec/requests/listings_endpoint_spec.rb
+++ spec/requests/listings_endpoint_spec.rb
@@ -23,6 +23,79 @@ RSpec.describe "Listings endpoint", :type => :request do
   end
 
   describe "prompt requirements" do
+
+    # Given that we have one Listing,
+    # with one Photo, which has one Image, which has one Thumb,
+    # and three tags, in an array...
+
+    let!(:listing) do
+      listing_attrs = {
+        uuid: "BoxStore123",
+        active: true,
+        name: "NewBoxStoreBoston",
+        email: "boxstore@boxstore.net",
+        description: "lots of information goes here!",
+        address: "524 Harrison Ave",
+        city: "Boston",
+        state: "MA",
+        zipcode: "23462",
+        lat: "40.7143528",
+        lng: "-74.0059731",
+        phone_number: "6179557522"
+      }
+      Listing.create(listing_attrs)
+    end
+
+    let!(:photo) do
+      photo_attrs = {
+        uuid: "902348586",
+        interest_point_id: 850924880,
+        name: "rsz_facefull.png",
+        listing: listing
+      }
+      Photo.create(photo_attrs)
+    end
+
+    let!(:image) do
+      image_attrs = {
+        url: "/uploads/photo/image/902348586/rsz_facefull.png",
+        photo: photo
+      }
+      Image.create(image_attrs)
+    end
+
+    let!(:thumb) do
+      thumb_attrs = {
+        url: "/uploads/photo/image/902348586/thumb_rsz_facefull.png",
+        image: image
+      }
+      Thumb.create(thumb_attrs)
+    end
+
+    let!(:tags) do
+      tag_attrs = [
+        {
+          label: "tag1"
+        },
+        {
+          label: "tag2"
+        },
+        {
+          label: "tag3"
+        }
+      ]
+      tag_attrs.map { |attrs| Tag.create(attrs) }
+    end
+
+    let!(:tag_associations) do
+      tags.map do |tag|
+        TagAssociation.create({
+          listing: listing,
+          tag: tag
+        })
+      end
+    end
+
     context "/listing/index" do
       describe "GET" do
         it "returns a specific array of listings"
